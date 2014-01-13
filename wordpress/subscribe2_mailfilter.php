e0d0a67b3212f0ac1db532642eb8c534890be2cb
Flemming Mahler <flemming@mahler.io>
2014-01-13 15:12:31 +0100

    Comments in English

--- wordpress/subscribe2_mailfilter.php
+++ wordpress/subscribe2_mailfilter.php
@@ -1,16 +1,15 @@
  <?php 
 add_filter('s2_registered_subscribers', function($registered, $args) { 
-	// $registered er et array med email adresser
+	// $registered is an array with email addresses
 	$activeUsers = new Array();
 
-	// Løb listen af mail-modtagere igennem...
 	foreach ($registered as $registeredEmail) { 
-		// Find denne brugers rolle i forhold til sitet.
+		// Find the current user role
 		$theUser = get_user_by( 'email', $registeredEmail );
 		$theUserRoles = $theUser->roles;
 		$userRole = array_shift($theUserRoles);
 
-		// Hvis brugeren ikke har en rolle, vil vi ikke håndtere vedkommende, men hvis de har en (admin, editor, author, ...), så skal de have mailen.
+		// If the user does not have a role, do not send email.
 		if (!empty($userRole )) {
 			array_push($activeUsers, $registeredEmail);
 		}
